51efe48cb4bc02ff07825939566a0151b01ad4a6
Mo Zhang <mozhangster@gmail.com>
2020-11-26 15:35:49 -0800

    A file was deleted

--- build/min_node/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/noetic/ros_ws/src/min_node
-# Build directory: /opt/ros/noetic/ros_ws/build/min_node
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/noetic/ros_ws/src/simulation
-# Build directory: /opt/ros/noetic/ros_ws/build/simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
